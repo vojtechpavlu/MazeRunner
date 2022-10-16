1d6adaf746ee44cbb14f871fb43e4b371cd28f2a
vojtechpavlu <vojtech.pavlu.dev>
2022-10-16 13:01:02 +0200

    Docstring fulfillment

--- src/algorithms/algorithm.py
+++ src/algorithms/algorithm.py
@@ -1,15 +1,36 @@
+"""Modul obsahuje abstraktní definici algoritmu. Díky abstrakční vrstvě, která
+převádí problem virtuálního bludiště do problému reprezentovaného jako graf,
+je možné použít algoritmy pro hledání cest v grafu.
 
+Společným předkem pro všechny implementace takových grafů je (v zájmu redukce
+redundance) abstraktní třída Algorithm.
+"""
 
+# Import prostředků pro definici abstraktních tříd a metod
 from abc import ABC, abstractmethod
 
-from src.state_space import StateSpace, Operator, State
+# Import tříd Stavového prostoru a stavu
+from src.state_space import StateSpace, State
 
 
 class Algorithm(ABC):
-    """"""
+    """Abstraktní třída Algorithm poskytuje společné služby pro všechny své
+    potomky, tedy algoritmy pro procházení grafů.
+
+    V zásadě musí být instance této třídy schopny udržovat stavový prostor a
+    poskytovat služby práce se seznamy uzlů k prohledání a již prohledaných.
+
+    Samotný běh algoritmu je definován abstraktní metodou `run()`, abstraktní
+    je dále i metoda pro poskytování dalšího uzlu k prohledání (každý
+    algoritmus se liší od ostatních mimo jiné i v tomto).
+    """
 
     def __init__(self, algo_name: str, state_space: StateSpace):
-        """"""
+        """Initor, který přijímá název algoritmu (pro rozlišovacích schopnosti)
+        a instanci stavového prostoru, v němž bude problém řešit.
+
+        Všichni potomci mají k těmto údajům přístup.
+        """
         self._algo_name = algo_name
         self._state_space = state_space
         self._fringe: list[State] = [state_space.initial_state]
--- src/algorithms/depth_first_search.py
+++ src/algorithms/depth_first_search.py
@@ -1,13 +1,24 @@
-""""""
+"""Modul obsahuje definici algoritmu Depth-First Search (DFS) pro prohledávání
+stavového prostoru tzv. po větvích.
+"""
 
 from src.algorithms.algorithm import Algorithm
 from src.state_space import StateSpace, State
 
 
 class DepthFirstSearch(Algorithm):
-    """"""
+    """Algoritmus DFS je slepým algoritmem pro prohledávání stavového prostoru.
+    Na obecné úrovni tento algoritmus negarantuje nalezení řešení, natož pak
+    optimálního.
+
+    Při použití na problém prohledávání uzavřeného a konečného bludiště však
+    řešení vždy najde (existuje-li nějaké), riziko suboptimality však trvá."""
 
     def __init__(self, state_space: StateSpace):
+        """Initor, který přijímá instanci stavového prostoru, v němž řešení
+        bude hledat.
+        """
+        # Volání initoru předka, tedy Algorithm.__init__(str, StateSpace)
         super().__init__("Depth First Search", state_space)
 
     @property
@@ -19,7 +30,25 @@ class DepthFirstSearch(Algorithm):
         return self._fringe.pop()
 
     def run(self) -> State:
-        """"""
+        """Implementace algoritmu DFS.
+
+        DFS se chápe fringe jako zásobník, tedy získává vždy poslední přidaný
+        prvek, který prohlašuje za aktuálně prohledávaný. Samotné prohledávání
+        stojí na:
+
+            - Otestování, zda-li není aktuální stav koncem
+            - Otestování, zda-li již nebyl aktuální stav prohledáván
+            - Přidáním všech svých potomků k prohledání v dalších iteracích.
+              Získání potomků je dosaženo pomocí aplikace všech dostupných
+              operátorů na aktuální stav. Pro konkrétní pravidla této filtrace
+              se podívejte na metodu `StateSpace.available_for_state(State)`
+
+        Pokud algoritmus prohledá všechny své stavy k prohledání a cíl nenajde,
+        je vyhozena výjimka, neboť řešení není dosažitelné.
+
+        Při přípravě instance předka (abstraktní třída Algorithm) je fringe
+        opatřena úvodním stavem - kořenem.
+        """
         while len(self.fringe) > 0:
             current_state = self.get_from_fringe
             if self.state_space.is_final_state(current_state):
