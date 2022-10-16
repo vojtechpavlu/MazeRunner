80d3eb036fdf60ce80ef52afb864ec9d22679ec7
vojtechpavlu <vojtech.pavlu.dev>
2022-10-16 12:31:31 +0200

    Abstract algorithm definition
    To be able to abstract from the problem itself (searching for path in maze) the abstraction layer was defined (StateSpace in this case). This architecture enables the use of general algorithms which can solve problems defined as graph.
    
    The mutual ancestor of these algorithms is this abstract algorithm.

---
+++ src/algorithms/algorithm.py
@@ -0,0 +1,68 @@
+
+
+from abc import ABC, abstractmethod
+
+from src.state_space import StateSpace, Operator, State
+
+
+class Algorithm(ABC):
+    """"""
+
+    def __init__(self, algo_name: str, state_space: StateSpace):
+        """"""
+        self._algo_name = algo_name
+        self._state_space = state_space
+        self._fringe: list[State] = [state_space.initial_state]
+        self._closed: list[State] = []
+
+    @property
+    def algorithm_name(self) -> str:
+        """Vlastnost vrací název algoritmu."""
+        return self._algo_name
+
+    @property
+    def state_space(self) -> StateSpace:
+        """Vrací stavový prostor, ve kterém má být prohledávání prováděno."""
+        return self._state_space
+
+    @property
+    def fringe(self) -> tuple[State]:
+        """Vlastnost vrací celý seznam prvků k rozevření."""
+        return tuple(self._fringe)
+
+    @property
+    def closed(self) -> tuple[State]:
+        """Vlastnost vrací celý seznam již rozevřených prvků."""
+        return tuple(self._closed)
+
+    def remember_state(self, state: State):
+        """Metoda si uloží dodaný stav do seznamu fringe (pro budoucí
+        prohledání)."""
+        self._fringe.append(state)
+
+    def is_in_closed(self, state: State) -> bool:
+        """Vlastnost vrací, zda-li je daný stav již obsažen v seznamu
+        prohledaných (closed)."""
+        return state in self.closed
+
+    def close_state(self, state: State):
+        """Metoda uloží dodaný stav do seznamu již prohledaných a uzavřených
+        stavů."""
+        self._closed.append(state)
+
+    @property
+    @abstractmethod
+    def get_from_fringe(self) -> State:
+        """Abstraktní metoda, která vrací element ze seznamu stavů k rozevření.
+        """
+
+    @abstractmethod
+    def run(self) -> State:
+        """Abstraktní metoda, která spouští algoritmus. Implementace této
+        metody sama o sobě definuje, jak bude algoritmus postupovat při
+        prohledávání."""
+
+
+
+
+
