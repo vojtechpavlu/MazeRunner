531bdca3413510032c20698d6094fb131201c6d7
vojtechpavlu <vojtech.pavlu.dev@gmail.com>
2022-11-04 21:45:22 +0100

    Basic testing environment

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from src.algorithms.algorithm import Success, Failure
 from src.printer import print_maze
 from src.state_space import StateSpace, State, Operator
 from src.maze import load_maze
@@ -23,27 +23,31 @@ algorithms = all_algorithms(ss)
 # Pro každý z algoritmů
 for algo in algorithms:
 
-    # Spusť časovač (v nanosekundách)
     start = time.time_ns()
+    final_state = None
 
-    # Spusť si algoritmus a získej cestu
-    final_state = algo.run()
+    try:
+        algo.run()
 
-    # Ukonči časovač (v nanosekundách)
-    end = time.time_ns()
+    except Success as s:
+        end = time.time_ns()
+        path = s.final_state.whole_path
+        print_maze(
+            maze.clone(),
+            [state.field.xy for state in s.final_state.all_states][1:]
+        )
 
-    # Nalezená cesta
-    path = final_state.whole_path
+        # Vypiš délku cesty a cestu
+        print(f"{algo.algorithm_name}, Počet kroků: {len(path)}")
+        print(f"Délka fringe a closed: {len(algo.fringe)}, {len(algo.closed)}")
+        print(f"Nalezená cesta: {path}")
+        print(f"Doba hledání cesty: {(end - start) / 10 ** 6} ms")
+        print(100 * "-", "\n")
 
-    print_maze(
-        maze.clone(),
-        [state.field.xy for state in final_state.all_states][1:]
-    )
-
-    # Vypiš délku cesty a cestu
-    print(f"{algo.algorithm_name}, Počet kroků: {len(path)}")
-    print(f"Délka fringe a closed: {len(algo.fringe)}, {len(algo.closed)}")
-    print(f"Nalezená cesta: {path}")
-    print(f"Doba hledání cesty: {(end - start) / 10 ** 6} ms")
-    print(100*"-", "\n")
+    except Failure as f:
+        print_maze(
+            maze.clone(),
+            [state.field.xy for state in f.last_valid_state.all_states][1:]
+        )
+        print(f"{algo.algorithm_name}: {f}")
 
